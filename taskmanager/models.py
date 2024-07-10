39956472614cccb64f1680d62b01d2a501e09f4f
Lewis Clements <lewisclements15@icloud.com>
2024-07-10 20:56:40 +0000

    add navbar

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,8 +1,8 @@
 from taskmanager import db
 
 
-class Category(db.model):
-    # schema for the category model
+class Category(db.Model):
+    # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
@@ -20,8 +20,9 @@ class Task(db.Model):
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
-    
+
     def __repr__(self):
+        # __repr__ to represent itself in the form of a string
         return "#{0} - Task: {1} | Urgent: {2}".format(
-        self.id, self.task_name, self.is_urgent
-    )     
+            self.id, self.task_name, self.is_urgent
+        )
